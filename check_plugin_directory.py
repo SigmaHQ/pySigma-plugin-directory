561d3d12b7058128c9736d055f3bc893d962c72e
Thomas Patzke <thomas@patzke.org>
2023-04-26 23:52:36 +0200

    Checks

---
+++ check_plugin_directory.py
@@ -0,0 +1,13 @@
+import json
+from sigma.plugins import SigmaPluginDirectory
+
+print("• Loading file")
+with open("pySigma-plugins-v1.json", "rt") as f:
+    directory = f.read()
+
+print("• Parsing JSON")
+def dupli
+parsed = json.loads(directory)
+
+print("• Parsing with pySigma")
+plugins = SigmaPluginDirectory.from_dict(parsed)
